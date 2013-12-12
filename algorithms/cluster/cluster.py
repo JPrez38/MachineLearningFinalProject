e2c64b5ec208801908ad873c81a71aed81cb7b37
shonteag <sag422@nyu.edu>
2013-12-12 18:11:40 -0500

    bug fix: first two features

--- algorithms/cluster/cluster.py
+++ algorithms/cluster/cluster.py
@@ -23,6 +23,7 @@ def printUsageAndExit(error):
 	print "Usage: kmeans.py [dataFile] [options (optional)]"
 	print "  Options:"
 	print "    --k #           Sets the number of clusters (default = 5)"
+	print "    --components #  Sets num_components for PCA (default = 3)"
 	print "-" * 64
 	sys.exit()
 #----------------------------------------------------------------------
@@ -47,17 +48,26 @@ def checkArgs():
 	else:
 		k = 5
 
+	if "--components" in sys.argv:
+		try:
+			components = int(sys.argv[sys.argv.index("--components")+1])
+		except:
+			printUsageAndExit("--components # must be an integer")
+	else:
+		components = 3
+
 	print "Program Arguments"
 	print "-" * 64
 	print "Data File:        " + str(sys.argv[1])
 	print "k =               " + str(k)
+	print "num_components =  " + str(components)
 	print "-" * 64
 
-	return reader,k
+	return reader,k,components
 #----------------------------------------------------------------------
 
 #check and get args
-reader,k = checkArgs()
+reader,k,components = checkArgs()
 
 
 print "Constructing data..."
@@ -66,16 +76,15 @@ print " -> " + str(len(data)) + " vectors generated\n"
 
 numpyData = np.array(data)
 
-pca = PCA(n_components=3)
+pca = PCA(n_components=components)
 reducedData = pca.fit_transform(numpyData)
-
-print reducedData
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
 #for j in range(0,k):
 ax.plot(reducedData[:,0],reducedData[:,1],reducedData[:,2],'o')
+plt.title("Unclustered Reduced Data, Plotted by First 3 PCs")
 plt.show()
 
 
@@ -92,11 +101,21 @@ for i in range(k):
 	lines = plt.plot(centroids[i,0],centroids[i,1],'kx')
 	plt.setp(lines,ms=15.0)
 	plt.setp(lines,mew=2.0)
-
+plt.title(str(k) + "-Means Clustered Reduced Data, Plotted by First 2 PCs")
 plt.show()
 
 
 #3d plot
 fig2 = plt.figure()
-ax2 = Axes3D*(fig2)
+ax2 = Axes3D(fig2)
+for j in range(k):
+	ds = reducedData[np.where(labels==j)]
+	ax2.plot(ds[:,0],ds[:,1],ds[:,2],'o')
+lines = ax2.plot(centroids[:,0],centroids[:,1],centroids[:,2],'kx')
+plt.setp(lines,ms=15.0)
+plt.setp(lines,mew=2.0)
+plt.title(str(k) + "-Means Clustered Reduced Data, Plotted by First 3 PCs")
+plt.show()
 
+#OUTPUTS
+for zip1,zip2 in zip(zip(data,outs),):
